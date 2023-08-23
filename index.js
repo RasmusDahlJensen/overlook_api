1ca8020e32323ea045ec58b4c0d994dffda8d593
Rasmus Dahl Jensen <62021203+RasmusDahlJensen@users.noreply.github.com>
2023-08-23 09:22:02 +0200

    Fixed cors

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
-import express from "express"
-import dotenv from 'dotenv'
-import { fileURLToPath } from 'url';
-import path from 'path';
+import express from "express";
+import dotenv from "dotenv";
+import { fileURLToPath } from "url";
+import path from "path";
+import InstallRouter from "./Routes/install.router.js";
+import CoreRouter from "./Routes/core.router.js";
+import AppRouter from "./Routes/app.router.js";
+import cors from "cors";
 
-import InstallRouter from "./Routes/install.router.js"
-import CoreRouter from "./Routes/core.router.js"
-import AppRouter from "./Routes/app.router.js"
+dotenv.config();
 
-dotenv.config()
+const port = process.env.PORT || 3000;
+const app = express();
 
-const port = process.env.PORT || 3000
-
-const app = express()
-
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 const currentUrl = import.meta.url;
 const currentPath = fileURLToPath(currentUrl);
 const currentDir = path.dirname(currentPath);
 
-// Define a route to serve static images
-app.use('/images', express.static(path.join(currentDir, 'images')));
+app.use(cors());
 
-app.use(InstallRouter)
-app.use(CoreRouter)
-app.use(AppRouter)
+// Define a route to serve static images
+app.use("/images", express.static(path.join(currentDir, "images")));
+
+app.use(InstallRouter);
+app.use(CoreRouter);
+app.use(AppRouter);
 
 app.listen(port, () => {
 	console.log(`Server kører på http://localhost:${port}`);
-})
+});
